b433121326c7a96acca39019fe9f34b4056bf672
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 10:36:01 -0500

    [chatRanks] non positional args

--- src/commands/settings/chatranks.ts
+++ src/commands/settings/chatranks.ts
@@ -20,7 +20,7 @@ function chatRanksHelp(player: Player, prefix: string, chatRanksBoolean: boolean
         `\n§o§4[§6Command§4]§f: chatranks`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: chatranks [options]`,
+        `§4[§6Usage§4]§f: ${prefix}chatranks [options]`,
         `§4[§6Description§4]§f: Toggles chat ranks.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -31,11 +31,6 @@ function chatRanksHelp(player: Player, prefix: string, chatRanksBoolean: boolean
         `       §4[§7Enable ChatRanks module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable ChatRanks module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}chatranks --help`,
-        `    ${prefix}chatranks --status`,
-        `    ${prefix}chatranks --enable`,
-        `    ${prefix}chatranks --disable`,
     ]);
 }
 
@@ -87,22 +82,27 @@ async function handleChatRanks(message: ChatSendAfterEvent, args: string[]) {
     const prefix = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
+                validFlagFound = true;
                 return chatRanksHelp(player, prefix, configuration.modules.chatranks.enabled, configuration.customcommands.chatranks);
             case "-s":
             case "--status":
                 // Handle status flag
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f ChatRanks module is currently ${configuration.modules.chatranks.enabled ? "enabled" : "disabled"}`);
                 break;
             case "-e":
             case "--enable":
                 // Handle enable flag
+                validFlagFound = true;
                 if (configuration.modules.chatranks.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f ChatRanks module is already enabled.`);
                 } else {
@@ -114,6 +114,7 @@ async function handleChatRanks(message: ChatSendAfterEvent, args: string[]) {
             case "-d":
             case "--disable":
                 // Handle disable flag
+                validFlagFound = true;
                 if (!configuration.modules.chatranks.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f ChatRanks module is already disabled.`);
                 } else {
@@ -122,12 +123,10 @@ async function handleChatRanks(message: ChatSendAfterEvent, args: string[]) {
                     sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${player.name}§f has disabled §4ChatRanks§f!`);
                 }
                 break;
-            default:
-                // Handle unrecognized flag
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid option. Use ${prefix}chatranks --help for more information.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         // No additional arguments provided, display help
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}chatranks --help for more information.`);
     }
