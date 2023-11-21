a859a1c0b94d52bc5b45360976276818e84a7a76
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 09:15:36 -0500

    [autoBan] non positional args

--- src/commands/settings/autoban.ts
+++ src/commands/settings/autoban.ts
@@ -21,7 +21,7 @@ function autobanHelp(player: Player, prefix: string, autoBanBoolean: boolean, se
         `\n§o§4[§6Command§4]§f: autoban`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: autoban [options]`,
+        `§4[§6Usage§4]§f: ${prefix}autoban [options]`,
         `§4[§6Description§4]§f: Toggles the AutoBan module, which automatically bans players based on their violations if above 50.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -32,11 +32,6 @@ function autobanHelp(player: Player, prefix: string, autoBanBoolean: boolean, se
         `       §4[§7Enable AutoBan module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable AutoBan module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}autoban --help`,
-        `    ${prefix}autoban --status`,
-        `    ${prefix}autoban --enable`,
-        `    ${prefix}autoban --disable`,
     ]);
 }
 
@@ -88,22 +83,27 @@ async function handleAutoban(message: ChatSendAfterEvent, args: string[]) {
     const prefix = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
+                validFlagFound = true;
                 return autobanHelp(player, prefix, configuration.modules.autoBan.enabled, configuration.customcommands.autoban);
             case "-s":
             case "--status":
                 // Handle status flag
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AutoBan module is currently ${configuration.modules.autoBan.enabled ? "enabled" : "disabled"}`);
                 break;
             case "-e":
             case "--enable":
                 // Handle enable flag
+                validFlagFound = true;
                 if (configuration.modules.autoBan.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AutoBan module is already enabled.`);
                 } else {
@@ -116,6 +116,7 @@ async function handleAutoban(message: ChatSendAfterEvent, args: string[]) {
             case "-d":
             case "--disable":
                 // Handle disable flag
+                validFlagFound = true;
                 if (!configuration.modules.autoBan.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AutoBan module is already disabled.`);
                 } else {
@@ -124,12 +125,10 @@ async function handleAutoban(message: ChatSendAfterEvent, args: string[]) {
                     sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${player.name}§f has disabled §4AutoBan§f!`);
                 }
                 break;
-            default:
-                // Handle unrecognized flag
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid option. Use ${prefix}autoban --help for more information.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         // No additional arguments provided, display help
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}autoban --help for more information.`);
     }
