6e2eb87176e74850506bf94a369bc366bae6a4e3
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 10:42:06 -0500

    [illegalEnchant] non positional args

--- src/commands/settings/illegalenchant.ts
+++ src/commands/settings/illegalenchant.ts
@@ -20,7 +20,7 @@ function illegalEnchantHelp(player: Player, prefix: string, illegalEnchantmentBo
         `\n§o§4[§6Command§4]§f: illegalenchant`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: illegalenchant [options]`,
+        `§4[§6Usage§4]§f: ${prefix}illegalenchant [options]`,
         `§4[§6Description§4]§f: Toggles checks for items with illegal enchantments.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -31,11 +31,6 @@ function illegalEnchantHelp(player: Player, prefix: string, illegalEnchantmentBo
         `       §4[§7Enable IllegalEnchantments module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable IllegalEnchantments module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}illegalenchant --help`,
-        `    ${prefix}illegalenchant --status`,
-        `    ${prefix}illegalenchant --enable`,
-        `    ${prefix}illegalenchant --disable`,
     ]);
 }
 
@@ -87,22 +82,27 @@ async function handleIllegalEnchant(message: ChatSendAfterEvent, args: string[])
     const prefix = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
+                validFlagFound = true;
                 return illegalEnchantHelp(player, prefix, configuration.modules.illegalEnchantment.enabled, configuration.customcommands.illegalenchant);
             case "-s":
             case "--status":
                 // Handle status flag
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f IllegalEnchantments module is currently ${configuration.modules.illegalEnchantment.enabled ? "enabled" : "disabled"}`);
                 break;
             case "-e":
             case "--enable":
                 // Handle enable flag
+                validFlagFound = true;
                 if (configuration.modules.illegalEnchantment.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f IllegalEnchantments module is already enabled.`);
                 } else {
@@ -114,6 +114,7 @@ async function handleIllegalEnchant(message: ChatSendAfterEvent, args: string[])
             case "-d":
             case "--disable":
                 // Handle disable flag
+                validFlagFound = true;
                 if (!configuration.modules.illegalEnchantment.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f IllegalEnchantments module is already disabled.`);
                 } else {
@@ -122,12 +123,10 @@ async function handleIllegalEnchant(message: ChatSendAfterEvent, args: string[])
                     sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${player.name}§f has disabled §4IllegalEnchantments§f!`);
                 }
                 break;
-            default:
-                // Handle unrecognized flag
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid option. Use ${prefix}illegalenchant --help for more information.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         // No additional arguments provided, display help
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}illegalenchant --help for more information.`);
     }
