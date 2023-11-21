fb6c0eacabb4a451a3b618edc9363a6877d9f5a1
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 09:00:26 -0500

    [antiScaffoldA] non positional args

--- src/commands/settings/antiscaffolda.ts
+++ src/commands/settings/antiscaffolda.ts
@@ -19,7 +19,7 @@ function antiscaffoldaHelp(player: Player, prefix: string, antiScaffoldABoolean:
         `\n§o§4[§6Command§4]§f: antiscaffolda`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: antiscaffolda [options]`,
+        `§4[§6Usage§4]§f: ${prefix}antiscaffolda [options]`,
         `§4[§6Description§4]§f: Monitors players for the usage of illegal scaffolding.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -30,11 +30,6 @@ function antiscaffoldaHelp(player: Player, prefix: string, antiScaffoldABoolean:
         `       §4[§7Enable AntiScaffoldA module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable AntiScaffoldA module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}antiscaffolda --help`,
-        `    ${prefix}antiscaffolda --status`,
-        `    ${prefix}antiscaffolda --enable`,
-        `    ${prefix}antiscaffolda --disable`,
     ]);
 }
 
@@ -81,22 +76,27 @@ async function handleAntiScaffoldA(message: ChatSendAfterEvent, args: string[]):
     const prefix = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
+                validFlagFound = true;
                 return antiscaffoldaHelp(player, prefix, configuration.modules.antiscaffoldA.enabled, configuration.customcommands.antiscaffolda);
             case "-s":
             case "--status":
                 // Handle status flag
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AntiScaffoldA module is currently ${configuration.modules.antiscaffoldA.enabled ? "enabled" : "disabled"}`);
                 break;
             case "-e":
             case "--enable":
                 // Handle enable flag
+                validFlagFound = true;
                 if (configuration.modules.antiscaffoldA.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AntiScaffoldA module is already enabled.`);
                 } else {
@@ -109,6 +109,7 @@ async function handleAntiScaffoldA(message: ChatSendAfterEvent, args: string[]):
             case "-d":
             case "--disable":
                 // Handle disable flag
+                validFlagFound = true;
                 if (!configuration.modules.antiscaffoldA.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AntiScaffoldA module is already disabled.`);
                 } else {
@@ -117,12 +118,10 @@ async function handleAntiScaffoldA(message: ChatSendAfterEvent, args: string[]):
                     sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${player.name}§f has disabled §4AntiScaffoldA§f!`);
                 }
                 break;
-            default:
-                // Handle unrecognized flag
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid option. Use ${prefix}antiscaffolda --help for more information.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}antiscaffolda --help for more information.`);
     }
 }
