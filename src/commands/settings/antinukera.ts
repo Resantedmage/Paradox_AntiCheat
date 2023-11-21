c6561181dc5744f598da5a5d76f69e8d1830fe22
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 08:53:15 -0500

    [antiNukerA] non positional args

--- src/commands/settings/antinukera.ts
+++ src/commands/settings/antinukera.ts
@@ -19,7 +19,7 @@ function antinukeraHelp(player: Player, prefix: string, antiNukerABoolean: boole
         `\n§o§4[§6Command§4]§f: antinukera`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: antinukera [options]`,
+        `§4[§6Usage§4]§f: ${prefix}antinukera [options]`,
         `§4[§6Description§4]§f: Monitors players for block nuking behavior.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -30,11 +30,6 @@ function antinukeraHelp(player: Player, prefix: string, antiNukerABoolean: boole
         `       §4[§7Enable AntiNukerA module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable AntiNukerA module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}antinukera --help`,
-        `    ${prefix}antinukera --status`,
-        `    ${prefix}antinukera --enable`,
-        `    ${prefix}antinukera --disable`,
     ]);
 }
 
@@ -81,22 +76,27 @@ async function handleAntiNukerA(message: ChatSendAfterEvent, args: string[]): Pr
     const prefix = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
+                validFlagFound = true;
                 return antinukeraHelp(player, prefix, configuration.modules.antinukerA.enabled, configuration.customcommands.antinukera);
             case "-s":
             case "--status":
                 // Handle status flag
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AntiNukerA module is currently ${configuration.modules.antinukerA.enabled ? "enabled" : "disabled"}`);
                 break;
             case "-e":
             case "--enable":
                 // Handle enable flag
+                validFlagFound = true;
                 if (configuration.modules.antinukerA.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AntiNukerA module is already enabled.`);
                 } else {
@@ -109,6 +109,7 @@ async function handleAntiNukerA(message: ChatSendAfterEvent, args: string[]): Pr
             case "-d":
             case "--disable":
                 // Handle disable flag
+                validFlagFound = true;
                 if (!configuration.modules.antinukerA.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AntiNukerA module is already disabled.`);
                 } else {
@@ -117,12 +118,10 @@ async function handleAntiNukerA(message: ChatSendAfterEvent, args: string[]): Pr
                     sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${player.name}§f has disabled §4AntiNukerA§f!`);
                 }
                 break;
-            default:
-                // Handle unrecognized flag
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid option. Use ${prefix}antinukera --help for more information.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}antinukera --help for more information.`);
     }
 }
