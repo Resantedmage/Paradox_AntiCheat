786aa5e3287a8a60903db49a7f93af3505c436d2
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 15:09:16 -0500

    [xrayA] non positional args

--- src/commands/settings/xraya.ts
+++ src/commands/settings/xraya.ts
@@ -21,7 +21,7 @@ function xrayAHelp(player: Player, prefix: string, xrayBoolean: boolean, setting
         `\n§o§4[§6Command§4]§f: xraya`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: xraya [options]`,
+        `§4[§6Usage§4]§f: ${prefix}xraya [options]`,
         `§4[§6Description§4]§f: Notifies staff when and where players mine specific ores.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -32,11 +32,6 @@ function xrayAHelp(player: Player, prefix: string, xrayBoolean: boolean, setting
         `       §4[§7Enable XrayA module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable XrayA module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}xraya --help`,
-        `    ${prefix}xraya --status`,
-        `    ${prefix}xraya --enable`,
-        `    ${prefix}xraya --disable`,
     ]);
 }
 
@@ -86,24 +81,29 @@ async function handleXrayA(message: ChatSendAfterEvent, args: string[]): Promise
     const prefix: string = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg: string = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
                 // Display help message
+                validFlagFound = true;
                 xrayAHelp(player, prefix, configuration.modules.xrayA.enabled, configuration.customcommands.xraya);
                 break;
             case "-s":
             case "--status":
                 // Display current status of XrayA module
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f XrayA module is currently ${configuration.modules.xrayA.enabled ? "§aENABLED" : "§4DISABLED"}§f.`);
                 break;
             case "-e":
             case "--enable":
                 // Enable XrayA module
+                validFlagFound = true;
                 if (!configuration.modules.xrayA.enabled) {
                     configuration.modules.xrayA.enabled = true;
                     dynamicPropertyRegistry.setProperty(undefined, "paradoxConfig", configuration);
@@ -116,6 +116,7 @@ async function handleXrayA(message: ChatSendAfterEvent, args: string[]): Promise
             case "-d":
             case "--disable":
                 // Disable XrayA module
+                validFlagFound = true;
                 if (configuration.modules.xrayA.enabled) {
                     configuration.modules.xrayA.enabled = false;
                     dynamicPropertyRegistry.setProperty(undefined, "paradoxConfig", configuration);
@@ -124,12 +125,10 @@ async function handleXrayA(message: ChatSendAfterEvent, args: string[]): Promise
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f XrayA module is already disabled`);
                 }
                 break;
-            default:
-                // Invalid argument
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid argument. Use ${prefix}xraya --help for command usage.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         // Invalid command
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}xraya --help for command usage.`);
     }
