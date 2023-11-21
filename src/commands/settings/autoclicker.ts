536be28b7c96340d4f0c6b52b6dfdcace001e5e3
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-11-21 09:17:15 -0500

    [autoClicker] non positional args

--- src/commands/settings/autoclicker.ts
+++ src/commands/settings/autoclicker.ts
@@ -21,7 +21,7 @@ function autoclickerHelp(player: Player, prefix: string, autoClickerBoolean: boo
         `\n§o§4[§6Command§4]§f: autoclicker`,
         `§4[§6Status§4]§f: ${commandStatus}`,
         `§4[§6Module§4]§f: ${moduleStatus}`,
-        `§4[§6Usage§4]§f: autoclicker [options]`,
+        `§4[§6Usage§4]§f: ${prefix}autoclicker [options]`,
         `§4[§6Description§4]§f: Toggles checks for players using autoclickers while attacking.`,
         `§4[§6Options§4]§f:`,
         `    -h, --help`,
@@ -32,11 +32,6 @@ function autoclickerHelp(player: Player, prefix: string, autoClickerBoolean: boo
         `       §4[§7Enable AutoClicker module§4]§f`,
         `    -d, --disable`,
         `       §4[§7Disable AutoClicker module§4]§f`,
-        `§4[§6Examples§4]§f:`,
-        `    ${prefix}autoclicker --help`,
-        `    ${prefix}autoclicker --status`,
-        `    ${prefix}autoclicker --enable`,
-        `    ${prefix}autoclicker --disable`,
     ]);
 }
 
@@ -88,22 +83,27 @@ async function handleAutoclicker(message: ChatSendAfterEvent, args: string[]) {
     const prefix = getPrefix(player);
 
     // Check for additional non-positional arguments
-    if (args.length > 0) {
-        const additionalArg = args[0].toLowerCase();
+    const length = args.length;
+    let validFlagFound = false; // Flag to track if any valid flag is encountered
+    for (let i = 0; i < length; i++) {
+        const additionalArg: string = args[i].toLowerCase();
 
         // Handle additional arguments
         switch (additionalArg) {
             case "-h":
             case "--help":
+                validFlagFound = true;
                 return autoclickerHelp(player, prefix, configuration.modules.autoclicker.enabled, configuration.customcommands.autoclicker);
             case "-s":
             case "--status":
                 // Handle status flag
+                validFlagFound = true;
                 sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AutoClicker module is currently ${configuration.modules.autoclicker.enabled ? "enabled" : "disabled"}`);
                 break;
             case "-e":
             case "--enable":
                 // Handle enable flag
+                validFlagFound = true;
                 if (configuration.modules.autoclicker.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AutoClicker module is already enabled.`);
                 } else {
@@ -116,6 +116,7 @@ async function handleAutoclicker(message: ChatSendAfterEvent, args: string[]) {
             case "-d":
             case "--disable":
                 // Handle disable flag
+                validFlagFound = true;
                 if (!configuration.modules.autoclicker.enabled) {
                     sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f AutoClicker module is already disabled.`);
                 } else {
@@ -124,12 +125,10 @@ async function handleAutoclicker(message: ChatSendAfterEvent, args: string[]) {
                     sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${player.name}§f has disabled §4AutoClicker§f!`);
                 }
                 break;
-            default:
-                // Handle unrecognized flag
-                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid option. Use ${prefix}autoclicker --help for more information.`);
-                break;
         }
-    } else {
+    }
+
+    if (!validFlagFound) {
         // No additional arguments provided, display help
         sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f Invalid command. Use ${prefix}autoclicker --help for more information.`);
     }
